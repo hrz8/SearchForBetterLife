2adeeccf1dbaf8e2af402840e062e73adf88c56a
Hirzi Nurfakhrian <63577087+hrz8@users.noreply.github.com>
2020-07-11 09:36:23 +0700

    Add project files.

---
+++ SearchForBetterLife/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.CustomerName = New System.Windows.Forms.TextBox()
+        Me.ProjectName = New System.Windows.Forms.TextBox()
+        Me.FileExtension = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FolderName = New System.Windows.Forms.TextBox()
+        Me.SelectFolderButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(417, 64)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(102, 20)
+        Me.SearchButton.TabIndex = 0
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'CustomerName
+        '
+        Me.CustomerName.Location = New System.Drawing.Point(15, 64)
+        Me.CustomerName.Name = "CustomerName"
+        Me.CustomerName.Size = New System.Drawing.Size(122, 20)
+        Me.CustomerName.TabIndex = 3
+        '
+        'ProjectName
+        '
+        Me.ProjectName.Location = New System.Drawing.Point(146, 64)
+        Me.ProjectName.Name = "ProjectName"
+        Me.ProjectName.Size = New System.Drawing.Size(122, 20)
+        Me.ProjectName.TabIndex = 4
+        '
+        'FileExtension
+        '
+        Me.FileExtension.Location = New System.Drawing.Point(280, 64)
+        Me.FileExtension.Name = "FileExtension"
+        Me.FileExtension.Size = New System.Drawing.Size(122, 20)
+        Me.FileExtension.TabIndex = 5
+        '
+        'FolderName
+        '
+        Me.FolderName.Location = New System.Drawing.Point(15, 13)
+        Me.FolderName.Name = "FolderName"
+        Me.FolderName.Size = New System.Drawing.Size(121, 20)
+        Me.FolderName.TabIndex = 6
+        '
+        'SelectFolderButton
+        '
+        Me.SelectFolderButton.Location = New System.Drawing.Point(146, 13)
+        Me.SelectFolderButton.Name = "SelectFolderButton"
+        Me.SelectFolderButton.Size = New System.Drawing.Size(122, 23)
+        Me.SelectFolderButton.TabIndex = 7
+        Me.SelectFolderButton.Text = "Select Folder"
+        Me.SelectFolderButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Customer"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(143, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Project"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(280, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Extension"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(15, 99)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(504, 183)
+        Me.RichTextBox1.TabIndex = 12
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 304)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SelectFolderButton)
+        Me.Controls.Add(Me.FolderName)
+        Me.Controls.Add(Me.FileExtension)
+        Me.Controls.Add(Me.ProjectName)
+        Me.Controls.Add(Me.CustomerName)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Name = "Form1"
+        Me.Text = "Search"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents CustomerName As TextBox
+    Friend WithEvents ProjectName As TextBox
+    Friend WithEvents FileExtension As TextBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents FolderName As TextBox
+    Friend WithEvents SelectFolderButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ SearchForBetterLife/Form1.vb
@@ -0,0 +1,32 @@
+﻿Option Strict On
+Imports System.IO
+
+Public Class Form1
+	Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+		Try
+			For Each dirItemTemp In Directory.GetFileSystemEntries(FolderName.Text, "*." & FileExtension.Text, SearchOption.AllDirectories) _
+					.Where(Function(file) file.IndexOf(ProjectName.Text, 0, StringComparison.CurrentCultureIgnoreCase) > -1 _
+										  AndAlso file.IndexOf(CustomerName.Text, 0, StringComparison.CurrentCultureIgnoreCase) > -1) _
+					.ToList()
+				Dim dirItem As String = dirItemTemp.Replace("\", "/")
+				RichTextBox1.AppendText("file://" & dirItem & vbNewLine & vbNewLine)
+				RichTextBox1.SelectionStart = Len(RichTextBox1.Text)
+				RichTextBox1.ScrollToCaret()
+				RichTextBox1.Select()
+			Next
+		Catch err As UnauthorizedAccessException
+			MsgBox(err)
+		End Try
+	End Sub
+
+	Private Sub SelectFolderButton_Click(sender As Object, e As EventArgs) Handles SelectFolderButton.Click
+		If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+			FolderName.Text = FolderBrowserDialog1.SelectedPath
+		End If
+	End Sub
+
+	Private Sub RichTextBox1_LinkClicked(sender As Object, e As LinkClickedEventArgs) Handles RichTextBox1.LinkClicked
+		Dim filepath = New Uri(e.LinkText, True).AbsolutePath
+		Process.Start(filepath)
+	End Sub
+End Class
